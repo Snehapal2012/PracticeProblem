a25d7d487538604b00b2e2be40b743b4ae906427
snehapal2012 <snehapal2012@gmail.com>
2022-06-19 12:00:24 +0530

    Added: Welcome to Practice Problem

---
+++ src/com/practiceproblem/PracticeProblemMain.java
@@ -0,0 +1,7 @@
+package com.practiceproblem;
+
+public class PracticeProblemMain {
+    public static void main(String[] args) {
+        System.out.println("Welcome To Practice Problem !");
+    }
+}
